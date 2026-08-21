5739ba6277fed5ab320d948e925337681d370646
Arvin <151423463+arvin16e@users.noreply.github.com>
2026-08-21 17:33:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 function volume_sphere() {
     //Write your code here
     let element=document.getElementById("radius");
-	if(element.textcontent==Number){
-		if(Number(element.textcontent) >=0 ){
-			let vol=(4/3)*Math.PI*Number(element.textcontent)**3;
-			document.createElement(
-				<p id='output' value="vol"></p>
-			);
+	if(element.value==Number){
+		if(Number(element.value) >=0 ){
+			let vol=(4/3)*Math.PI*Number(element.value)**3;
+			document.getElementById("volume").value=vol;
 		}else{
-			document.createElement(
-				<p id='output1' value="NaN"></p>
-			);
+			document.getElementById("volume").value="NaN";
 		}
 		
 		
 	}else{
-		document.createElement(
-				<p id='output2' value="NaN"></p>
-			);
+		document.getElementById("volume").value="NaN";
 	}
 	
 } 
